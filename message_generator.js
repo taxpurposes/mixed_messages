6c8d0beab73a47b004f0f7aba17e7751f6fbaeea
taxpurposes <danjzych@gmail.com>
2022-03-27 23:07:15 -0500

    Added list of possible sentence pieces. Added functions to randomly select each.

--- message_generator.js
+++ message_generator.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+//arrays containing different message components. desires = different states of wanting, foods = different potential meals, and deciders = whether or not it was eaten.
+
+const desires = ['Today, I really wanted ', 'I\'ve never tried eating ', 'Yesterday I felt like having some '];
+
+const foods = ['braised beef vegan ramen, ', 'paella, ', 'horny goat weed soup, ', 'Rocky Mountain Oysters, '];
+
+const deciders = ['so I ate a bunch of it when I went out to eat with my friends.', 'so I ate a shit ton of it.', 'so I ate a bunch of it today.', 'but I didn\'t get to eat any of it today'];
+
+const decide_desire = () => {
+    return Math.floor(Math.random() *3);
+};
+
+const decide_food = () => {
+    return Math.floor(Math.random() *5);
+};
+
+const decide_deciders = () => {
+    return Math.floor(Math.random() *5)
+}
