dc7989aa89e01d94a968cfc1f4c9f746f8136db1
taxpurposes <danjzych@gmail.com>
2022-03-27 23:14:37 -0500

    finished message generator woohoo

--- message_generator.js
+++ message_generator.js
@@ -4,16 +4,28 @@ const desires = ['Today, I really wanted ', 'I\'ve never tried eating ', 'Yester
 
 const foods = ['braised beef vegan ramen, ', 'paella, ', 'horny goat weed soup, ', 'Rocky Mountain Oysters, '];
 
-const deciders = ['so I ate a bunch of it when I went out to eat with my friends.', 'so I ate a shit ton of it.', 'so I ate a bunch of it today.', 'but I didn\'t get to eat any of it today'];
+const deciders = ['so I ate a bunch of it when I went out to eat with my friends.', 'so I ate a shit ton of it.', 'so I ate a bunch of it today.', 'but I didn\'t get to eat any of it today :('];
 
 const decide_desire = () => {
     return Math.floor(Math.random() *3);
 };
 
 const decide_food = () => {
-    return Math.floor(Math.random() *5);
+    return Math.floor(Math.random() *4);
 };
 
 const decide_deciders = () => {
-    return Math.floor(Math.random() *5)
+    return Math.floor(Math.random() *4)
+};
+
+const generate_food_desire = () => {
+    let desire = decide_desire();
+    let food = decide_food();
+    let decision = decide_deciders();
+    desire = desires[desire];
+    food = foods[food];
+    decision = deciders[decision];
+    console.log(desire + food + decision)
 }
+
+generate_food_desire()
